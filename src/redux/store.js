846db3b6ab7e39da91cee70b1e760bf358252199
Bohdan Lytvynets <157913587+Bohdan-Lytvynets@users.noreply.github.com>
2025-05-23 17:48:20 +0200

    finished work

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authReducer } from "./auth/authSlice";
+import balanceReducer from './slices/balanceSlice';
 
 export const store = configureStore({
   reducer: {
-    auth: authReducer
+    auth: authReducer,
+    balance: balanceReducer,
+    
   }
 });
 

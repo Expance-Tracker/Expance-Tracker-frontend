54d4cdd5811a4eb6515970f166c16e2def8cd04c
Volodymyr Derhilov <v.dergilov@gmail.com>
2025-05-23 16:24:48 +0300

    Add saving token in ls while login

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,24 @@ import DashboardPage from "./pages/DashboardPage/DashboardPage";
 import Header from "./components/Header/Header";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RegistrationForm from "./components/RegistrationForm/RegistrationForm";
+import RestrictedRoute from "./components/routes/RestrictedRoute";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<LoginPage />} />
+      <Route
+        path="/"
+        element={
+          <RestrictedRoute redirectTo="/dashboard" component={<LoginPage />} />
+        }
+      />
       <Route path="/register" element={<RegistrationForm />} />
-      <Route path="/login" element={<LoginPage />} />
+      <Route
+        path="/login"
+        element={
+          <RestrictedRoute redirectTo="/dashboard" component={<LoginPage />} />
+        }
+      />
       <Route path="/dashboard" element={<DashboardPage />} />
       {/* Щоб побачити Header, у верхньому element, замість <DashboardPage /> прописати <Header /> та перейти в браузере на роут "/dashboard". Тому що, header може бачити тільки логінізований користувач, а логіка з логіном поки що не прописана */}
     </Routes>

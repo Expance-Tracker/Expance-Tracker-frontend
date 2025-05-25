7a897b1df4139fed3439bef9d243aa7a893ee507
AndriiKokariev <andrewkok657@gmail.com>
2025-05-25 15:17:20 +0200

    completing form

--- src/components/AddTransactionForm/AddTransactionForm.jsx
+++ src/components/AddTransactionForm/AddTransactionForm.jsx
@@ -14,15 +14,40 @@ const AddTransactionForm = ({ onClose }) => {
     <div>
       <Formik>
         <Form>
-          <Field type="checkbox"></Field>
-          <Field as="select"></Field>
-          <Field type="number"></Field>
-          <Field type="date"></Field>
-          <Field></Field>
-          <button type="submit">Add</button>
+          <Field type="checkbox" name="type"></Field>
+          <div className={s.inputsContainer}>
+            <Field as="select" name="category">
+              <option value="">Category</option>
+              <option value="Main expenses">Main expenses</option>
+              <option value="Products">Products</option>
+              <option value="Car">Car</option>
+              <option value="Self care">Self care</option>
+              <option value="Child care">Child care</option>
+              <option value="Household products">Household products</option>
+              <option value="Education">Education</option>
+              <option value="Leisure">Leisure</option>
+            </Field>
+            <div className={s.smallContainer}>
+              <Field
+                className={s.amountInput}
+                type="number"
+                name="amount"
+                placeholder="0.00"
+              ></Field>
+              <Field className={s.dateInput} type="date" name="date"></Field>
+            </div>
+            <Field
+              className={s.comment}
+              placeholder="Comment"
+              name="comment"
+            ></Field>
+          </div>
+          <button className={s.submitBtn} type="submit">
+            Add
+          </button>
         </Form>
       </Formik>
-      <button onClick={onClose} type="button">
+      <button className={s.closeBtn} onClick={onClose} type="button">
         Cancel
       </button>
     </div>

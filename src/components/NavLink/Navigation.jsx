c786fef7a6a9230579c2e4ad834c572a8d72e7e3
mashukak <mashenkast12@gmail.com>
2025-05-26 01:14:30 +0200

    add svg to Navi (not last version)

--- src/components/NavLink/Navigation.jsx
+++ src/components/NavLink/Navigation.jsx
@@ -1,39 +1,46 @@
 import { NavLink } from 'react-router-dom';
+import { HomeIcon, StatsIcon, CurrencyIcon } from '../Svg/SvgNavi';
 import styles from './Navigation.module.css';
 
 const Navigation = () => {
   return (
     <nav className={styles.navigationContainer}>
       <ul className={styles.navList}>
-        <li>
+        <li className={styles.navItem}>
           <NavLink 
             to="/" 
             end 
-            className={({ isActive }) => 
-              isActive ? `${styles.navLink} ${styles.navLinkActive}` : styles.navLink
-            }
+            className={({isActive}) => isActive ? 
+              `${styles.navLink} ${styles.navLinkActive}` : styles.navLink}
           >
-            Home
+            <div className={styles.navIcon}>
+              <HomeIcon width={18} height={18} style={{ display: 'block' }} />
+            </div>
+            <span>Home</span>
           </NavLink>
         </li>
-        <li>
+        <li className={styles.navItem}>
           <NavLink 
             to="/statistics" 
-            className={({ isActive }) => 
-              isActive ? `${styles.navLink} ${styles.navLinkActive}` : styles.navLink
-            }
+            className={({isActive}) => isActive ? 
+              `${styles.navLink} ${styles.navLinkActive}` : styles.navLink}
           >
-            Statistics
+            <div className={styles.navIcon}>
+              <StatsIcon width={18} height={18} style={{ display: 'block' }} />
+            </div>
+            <span>Statistics</span>
           </NavLink>
         </li>
-        <li className={styles.currencyTab}>
+        <li className={`${styles.navItem} ${styles.currencyTab}`}>
           <NavLink 
             to="/currency" 
-            className={({ isActive }) => 
-              isActive ? `${styles.navLink} ${styles.navLinkActive}` : styles.navLink
-            }
+            className={({isActive}) => isActive ? 
+              `${styles.navLink} ${styles.navLinkActive}` : styles.navLink}
           >
-            Currency
+            <div className={styles.navIcon}>
+              <CurrencyIcon width={18} height={18} style={{ display: 'block' }} />
+            </div>
+            <span>Currency</span>
           </NavLink>
         </li>
       </ul>

224c665b868b6afb0c44434cebf18f169d9505e6
mashukak <mashenkast12@gmail.com>
2025-05-25 22:15:55 +0200

    add Loader last attempt

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
-
 import { Routes, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
 import RegistrationForm from "./components/RegistrationForm/RegistrationForm";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import DashboardPage from "./pages/DashboardPage/DashboardPage";
@@ -8,30 +8,36 @@ import LogoutModal from "./components/LogoutModal/LogoutModal";
 import Header from "./components/Header/Header";
 import RestrictedRoute from "./components/routes/RestrictedRoute";
 import HomeTab from "./pages/HomeTab/HomeTab";
+import Loader from "./components/Loader/Loader";
 
 function App() {
-  return (
-    <Routes>
-      {/* Public Routes */}
-      <Route path="/register" element={<RegistrationForm />} />
-      <Route
-        path="/login"
-        element={<RestrictedRoute redirectTo="/" component={<LoginPage />} />}
-      />
+  const isLoading = useSelector(state => state.global?.isLoading ?? false);
 
-      {/* Protected Routes Layout */}
-      <Route
-        path="/"
-        element={
-          <>
-            <Header />
-            <LogoutModal />
-            {/* <DashboardPage /> */}
-            <HomeTab />
-          </>
-        }
-      />
-    </Routes>
+  return (
+    <>
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/register" element={<RegistrationForm />} />
+        <Route
+          path="/login"
+          element={<RestrictedRoute redirectTo="/" component={<LoginPage />} />}
+        />
+
+        {/* Protected Routes Layout */}
+        <Route
+          path="/"
+          element={
+            <>
+              <Header />
+              <LogoutModal />
+              <HomeTab />
+            </>
+          }
+        />
+      </Routes>
+
+      {isLoading && <Loader />}
+    </>
   );
 }
 

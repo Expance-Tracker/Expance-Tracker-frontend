45c7f50b9af7dfbb2f7990a1c65419f6cfb012da
mashukak <mashenkast12@gmail.com>
2025-05-25 21:40:42 +0200

    fix vercel

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,22 +1,14 @@
-import { ThreeDots } from 'react-loader-spinner';
+import { ClipLoader } from 'react-spinners';
 import './Loader.module.css';
 
 const Loader = () => {
   return (
     <div className="loader-overlay">
-      <ThreeDots 
-        height="80"
-        width="80"
-        radius="9"
-        color="#FF5733" 
-        secondaryColor="#3498db"
-        ariaLabel="three-dots-loading"
-        visible={true}
-        wrapperStyle={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}
+      <ClipLoader
+        color="#FF5733"
+        size={80}
+        speedMultiplier={1}
+        aria-label="Loading Spinner"
       />
     </div>
   );

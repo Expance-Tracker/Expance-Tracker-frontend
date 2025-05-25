cc463de9dd3937a4ce85e2775bc7e61d14e164d2
Bibosss <savoniksasha7@gmail.com>
2025-05-25 17:21:29 +0300

    fix-token

--- src/redux/transactions/operations.js
+++ src/redux/transactions/operations.js
@@ -3,20 +3,12 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 
 axios.defaults.baseURL = "https://expance-tracker-backend-9zu7.onrender.com";
 
-const ensureToken = () => {
-  if (!axios.defaults.headers.common.Authorization) {
-    const token = localStorage.getItem("token");
-    if (token) {
-      axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-    }
-  }
-};
-
 export const getTransactions = createAsyncThunk(
   "transactions/get",
   async (_, thunkAPI) => {
     try {
-      const token = localStorage.getItem("token");
+      const state = thunkAPI.getState();
+      const token = state.auth.token;
 
       if (!token) {
         return thunkAPI.rejectWithValue("No token found");
@@ -27,8 +19,9 @@ export const getTransactions = createAsyncThunk(
       const { data } = await axios.get("/transactions");
       return data.data;
     } catch (err) {
-      return thunkAPI.rejectWithValue(err.message);
+      return thunkAPI.rejectWithValue(
+        err.response?.data?.message || err.message
+      );
     }
   }
 );
-  

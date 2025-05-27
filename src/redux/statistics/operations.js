76f9a56e23e4a0004065fd4585e006a188865a48
vanyaandrienko <vanyaandrienko@gmail.com>
2025-05-27 22:29:48 +0300

    fix yymm

--- src/redux/statistics/operations.js
+++ src/redux/statistics/operations.js
@@ -7,10 +7,15 @@ export const fetchStatistics = createAsyncThunk(
       try {
         const state = thunkAPI.getState();
         const token = state.auth.token || state.auth.value?.token;
+        
+        const monthString = `${month.toString().padStart(2, '0')}-${year}`;
         const response = await axios.get(
           "https://expance-tracker-backend-9zu7.onrender.com/statistics/summary",
           {
-            params: { type, month, year },
+            params: {
+              type,              
+              month: monthString 
+            },
             headers: {
               Authorization: `Bearer ${token}`,
             },

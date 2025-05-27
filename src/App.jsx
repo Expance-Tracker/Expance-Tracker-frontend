f3697bc39af0c3679708b7413bdd08564a5c6c03
AlexKaplan <alexkaplan110606@gmail.com>
2025-05-27 06:41:29 +0300

    fix(app): fixed import

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,18 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 
 import CurrencyTab from "./pages/CurrencyTab/CurrencyTab";
+import Delete from "./components/Delete/Delete";
 import Header from "./components/Header/Header";
 import HomeTab from "./pages/HomeTab/HomeTab";
 import Loader from "./components/Loader/Loader";
-
 import LoginPage from "./pages/LoginPage/LoginPage";
 import LogoutModal from "./components/LogoutModal/LogoutModal";
-
-import Delete from "./components/Delete/Delete";
-
 import Navigation from "./components/NavLink/Navigation";
-
+import PrivateRoute from "./components/routes/PrivateRoute";
 import RegistrationForm from "./components/RegistrationForm/RegistrationForm";
 import RestrictedRoute from "./components/routes/RestrictedRoute";
 import StatisticsTab from "./pages/StatisticsTab/StatisticsTab";
 import { useSelector } from "react-redux";
-
-import CurrencyTab from "./pages/CurrencyTab/CurrencyTab";
-import PrivateRoute from "./components/routes/PrivateRoute";
-
 
 function App() {
   const isLoading = useSelector((state) => state.global?.isLoading ?? false);
@@ -75,7 +68,6 @@ function App() {
             <PrivateRoute>
               <Header />
               <div className="page-content">
-                
                 <CurrencyTab />
               </div>
               <LogoutModal />

1aa1f800c52822f75d65f98227e3371d1a7752cc
mashukak <mashenkast12@gmail.com>
2025-05-28 09:23:23 +0200

    add right styles to statistic

--- src/pages/StatisticsTab/StatisticsTab.jsx
+++ src/pages/StatisticsTab/StatisticsTab.jsx
@@ -5,60 +5,71 @@ import {
   setMonth,
   setYear,
 } from "../../redux/statistics/statisticsSlice";
-import { fetchStatistics } from "../../redux/statistics/operations";
-import StatisticsDashboard from "../../components/statistics/StatisticsDashboard/StatisticsDashboard";
-import Toggle from "../../components/statistics/Toggle/Toggle";
-import Chart from "../../components/statistics/Chart/Chart";
-import StatisticsTable from "../../components/statistics/StatisticsTable/StatisticsTable";
+import { fetchStatistics } from "../../redux/Statistics/operations";
+import StatisticsDashboard from "../../components/Statistics/StatisticsDashboard/StatisticsDashboard";
+import Toggle from "../../components/Statistics/Toggle/Toggle";
+import Chart from "../../components/Statistics/Chart/Chart";
+import StatisticsTable from "../../components/Statistics/StatisticsTable/StatisticsTable";
 import styles from "./StatisticsTab.module.css";
 import Currency from '../../components/Currency/Currency';
 import Navigation from '../../components/NavLink/Navigation';
 import { setLoading } from "../../redux/global/globalSlice";
+import Balance from "../../components/Balance/Balance";
 
 const StatisticsTab = () => {
   const dispatch = useDispatch();
   const { data, isLoading, type, month, year, error } = useSelector(
     (state) => state.statistics
   );
+
   useEffect(() => {
     dispatch(setLoading(true));
     const timer = setTimeout(() => dispatch(setLoading(false)), 2000);
     return () => clearTimeout(timer);
-  }, [dispatch]); 
+  }, [dispatch]);
+
   useEffect(() => {
     dispatch(fetchStatistics({ type, month, year }));
   }, [dispatch, type, month, year]);
 
   return (
-    <div>
-    <Navigation />
-    <div className={styles.currency_not_mobile}>
-      <Currency />
-      </div>
-      <section className={styles.statisticsSection}>
-      <div className={styles.statisticsContainer}>
-        <div className={styles.chartSide}>
-          <Toggle type={type} setType={(v) => dispatch(setType(v))} />
-          <Chart type={type} data={data} isLoading={isLoading} />
-        </div>
-        <div className={styles.statsSide}>
-          <StatisticsDashboard
-            month={month}
-            year={year}
-            onMonthChange={(v) => dispatch(setMonth(v))}
-            onYearChange={(v) => dispatch(setYear(v))}
-          />
-          {error && <div style={{ color: "red" }}>{error}</div>}
-          <StatisticsTable type={type} data={data} isLoading={isLoading} />
+    <div className={styles.container}>
+      <div className={styles.box_without_chart}>
+        <div className={styles.without_chart_content}>
+          <div className={styles.box_navigation_balance}>
+          
+              <Navigation />
+              <div className={styles.balance_deskopt}>
+                <Balance />
+              </div>
+            </div>
+              <div className={styles.currency_mobile}>
+              <Currency />
+            
+          </div>
         </div>
       </div>
-    </section>
-  </div>
-    
+      
+      <div className={styles.statisticsSection}>
+        <section className={styles.statisticsContainer}>
+          <div className={styles.chartSide}>
+            <Toggle type={type} setType={(v) => dispatch(setType(v))} />
+            <Chart type={type} data={data} isLoading={isLoading} />
+          </div>
+          <div className={styles.statsSide}>
+            <StatisticsDashboard
+              month={month}
+              year={year}
+              onMonthChange={(v) => dispatch(setMonth(v))}
+              onYearChange={(v) => dispatch(setYear(v))}
+            />
+            {error && <div style={{ color: "red" }}>{error}</div>}
+            <StatisticsTable type={type} data={data} isLoading={isLoading} />
+          </div>
+        </section>
+      </div>
+    </div>
   );
 };
 
-
-
 export default StatisticsTab;
-
